10674197cf418659b2472fb157957f15125f33ca
Zoom NGUYEN <frtlupsvn@gmail.com>
2015-03-19 16:21:51 +0700

    last commit

--- Hack Your Brain/GameScene.swift
+++ Hack Your Brain/GameScene.swift
@@ -7,11 +7,13 @@
 //
 
 import SpriteKit
+import AVFoundation
 
 class GameScene: SKScene {
     
     var myLabel = SKLabelNode()
     var myScore = SKLabelNode()
+    var myHighestScore = SKLabelNode()
     
     var myCubic = SKSpriteNode()
     
@@ -25,12 +27,45 @@ class GameScene: SKScene {
     var ballYellowTexture = SKTexture(imageNamed: "yellow.png")
     var ballRedTexture = SKTexture(imageNamed: "red.png")
     
-    
+    var player = AVAudioPlayer()
+
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        //Sound
+
+        // init Audio Player
+        var alertSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bgSound", ofType: "mp3")!)
+        // Removed deprecated use of AVAudioSessionDelegate protocol
+        AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error: nil)
+        AVAudioSession.sharedInstance().setActive(true, error: nil)
+        
+        var error:NSError?
+        player = AVAudioPlayer(contentsOfURL: alertSound, error: &error)
+        player.prepareToPlay()
+        
+        
+        
         scoreValue = 0
         cubicValue = 1 // Meen yellow color
-        speedGame = 2
+        speedGame  = 2
+        
+        //My Highest Score
+        myHighestScore = SKLabelNode(fontNamed:"Chalkduster")
+        myHighestScore.color = SKColor.blueColor()
+        myHighestScore.colorBlendFactor = 1
+        myHighestScore.fontSize = 30;
+        
+        println(CGRectGetMidX(self.frame))
+        myHighestScore.position = CGPoint(x: CGRectGetMidX(self.frame) - 100 , y: self.frame.height - 40)
+        if (NSUserDefaults.standardUserDefaults().objectForKey("highestScore") != nil) {
+            myHighestScore.text = String(format: "Record: %d",NSUserDefaults.standardUserDefaults().objectForKey("highestScore") as Int )
+        }else {
+            NSUserDefaults.standardUserDefaults().setObject(0, forKey: "highestScore")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            myHighestScore.text = String(format: "Record: %d",NSUserDefaults.standardUserDefaults().objectForKey("highestScore") as Int )
+        }
+        self.addChild(myHighestScore)
         
         backgroundColor = UIColor.whiteColor()
         writeScore(String(scoreValue))
@@ -48,9 +83,10 @@ class GameScene: SKScene {
     }
     func resetGame(){
         /* Setup your scene here */
-        scoreValue = 0
-        cubicValue = 1 // Meen yellow color
-        speedGame = 2
+        scoreValue  = 0
+        cubicValue  = 1 // Meen yellow color
+        speedGame   = 2
+        self.player.currentTime = 0
         myLabel.removeFromParent()
         myCubic.removeFromParent()
         
@@ -67,15 +103,16 @@ class GameScene: SKScene {
         
         
         var countdown = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("countdown"), userInfo: nil, repeats: false)
-
+        
     }
     func writeScore(text:NSString){
+        
         myScore = SKLabelNode(fontNamed:"Chalkduster")
         myScore.text = text
         myScore.color = SKColor.redColor()
         myScore.colorBlendFactor = 1
-        myScore.fontSize = 30;
-        myScore.position = CGPoint(x:CGRectGetMidX(self.frame)-100, y:(CGRectGetMidY(self.frame) + 200));
+        myScore.fontSize = 80;
+        myScore.position = CGPoint(x:CGRectGetMidX(self.frame)-100, y:(CGRectGetMidY(self.frame)+200));
         self.addChild(myScore)
     }
     func writeSomeThing(text:NSString){
@@ -83,7 +120,7 @@ class GameScene: SKScene {
         myLabel.text = text
         myLabel.color = SKColor.blackColor()
         myLabel.colorBlendFactor = 1
-        myLabel.fontSize = 65;
+        myLabel.fontSize = 40;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         self.addChild(myLabel)
     }
@@ -103,45 +140,59 @@ class GameScene: SKScene {
     
     
     func gameStart(){
+        player.play()
         myLabel.removeFromParent()
         ballValue = randomBallValue(1, max: 4)
         
-            //Game is Started
-            var ball = SKSpriteNode()
+        //Game is Started
+        var ball = SKSpriteNode()
+        
+        switch ballValue{
+        case 1:
+            ball = SKSpriteNode(texture: ballYellowTexture)
+        case 2:
+            ball = SKSpriteNode(texture: ballBlueTexture)
+        case 3:
+            ball = SKSpriteNode(texture: ballGreenTexture)
+        case 4:
+            ball = SKSpriteNode(texture: ballRedTexture)
+        default:
+            break
+        }
+        
+        ball.size = CGSizeMake(self.frame.width / 15 ,self.frame.width / 15)
+        ball.position = CGPoint(x: CGRectGetMidX(self.frame) , y: (CGRectGetMidY(self.frame) + 200))
+        ball.zPosition = -1
+        self.addChild(ball)
+        
+        //Ball drop down
+        var moveBottom = SKAction.moveToY((CGRectGetMidY(self.frame) - 200 + (self.frame.width / 8)), duration: speedGame)
+        ball.runAction(moveBottom, completion: {
+            ball.removeFromParent()
             
-            switch ballValue{
-            case 1:
-                ball = SKSpriteNode(texture: ballYellowTexture)
-            case 2:
-                ball = SKSpriteNode(texture: ballBlueTexture)
-            case 3:
-                ball = SKSpriteNode(texture: ballGreenTexture)
-            case 4:
-                ball = SKSpriteNode(texture: ballRedTexture)
-            default:
-                break
-            }
-            
-            ball.size = CGSizeMake(self.frame.width / 15 ,self.frame.width / 15)
-            ball.position = CGPoint(x: CGRectGetMidX(self.frame) , y: (CGRectGetMidY(self.frame) + 200))
-            ball.zPosition = -1
-            self.addChild(ball)
-            
-            //Ball drop down
-            var moveBottom = SKAction.moveToY((CGRectGetMidY(self.frame) - 200 + (self.frame.width / 8)), duration: speedGame)
-            ball.runAction(moveBottom, completion: {
-                ball.removeFromParent()
+            if(self.checkScored(self.cubicValue, ball: self.ballValue)){
                 
-                if(self.checkScored(self.cubicValue, ball: self.ballValue)){
-                    self.scoreValue += 1
-                    self.myScore.text = String(self.scoreValue)
-                    self.speedGame -= 0.05
-                    self.gameStart()
-                } else {
-                    self.writeSomeThing("Thua Me roi")
-                    var countdown = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("resetGame"), userInfo: nil, repeats: false)
+                self.scoreValue += 1
+                self.myScore.text = String(self.scoreValue)
+                self.speedGame -= 0.02
+                self.gameStart()
+            } else {
+                
+                
+                if (self.scoreValue > NSUserDefaults.standardUserDefaults().objectForKey("highestScore") as Int){
+                    NSUserDefaults.standardUserDefaults().setObject(self.scoreValue, forKey: "highestScore")
+                    NSUserDefaults.standardUserDefaults().synchronize()
+                    self.myHighestScore.text = String(format: "Record: %d",NSUserDefaults.standardUserDefaults().objectForKey("highestScore") as Int)
+                    self.writeSomeThing("New Record !")
+                    self.player.stop()
+                }else {
+                    self.writeSomeThing("You Lose !")
+                    self.player.stop()
                 }
-            })
+                
+                var countdown = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("resetGame"), userInfo: nil, repeats: false)
+            }
+        })
         
     }
     
